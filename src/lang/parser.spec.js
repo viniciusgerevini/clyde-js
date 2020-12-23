cfa356aa8764a6bc3993dc3b3e4ab8cf05f31a03
Vinicius Gerevini <viniciusgerevini@gmail.com>
2020-12-23 19:57:36 +1100

    added compilation tests

---
+++ src/lang/parser.spec.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const parser = require('./parser');
+
+describe('Check compilation results', () => {
+  const EXAMPLES_FOLDER = './examples/';
+  const RESULTS_FOLDER = './examples/results/';
+
+  const getSourceFiles = () => {
+    return fs.readdirSync(EXAMPLES_FOLDER, { withFileTypes: true })
+      .filter(f => f.isFile() && f.name.endsWith('.clyde'))
+      .map( f => f.name );
+  }
+
+  const getFile = (path) => fs.readFileSync(path, "utf8");
+
+  const getSourceFile = (sourceFileName) => {
+    return getFile(`${EXAMPLES_FOLDER}${sourceFileName}`);
+  };
+
+  const getExpectedResult = (sourceFileName) => {
+    return getFile(`${RESULTS_FOLDER}${sourceFileName.replace(/\.clyde$/, '.json')}`);
+  };
+
+  test.each(getSourceFiles())('check: %s', (sourceFileName) => {
+    const source = getSourceFile(sourceFileName);
+    const expectedResult = getExpectedResult(sourceFileName);
+    const result = parser.parse(source);
+
+    expect(JSON.parse(JSON.stringify(result))).toEqual(JSON.parse(expectedResult));
+  });
+});
