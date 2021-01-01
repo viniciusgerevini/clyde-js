772b6e78d4941f21e8ccdaa44fa09ff125c5e21e
Vinicius Gerevini <viniciusgerevini@gmail.com>
2021-01-01 15:49:31 +1100

    implemented interpreter cli

--- interpreter/scripts/cli.js
+++ interpreter/scripts/cli.js
@@ -1,12 +1,76 @@
 const fs = require('fs');
+const readline = require('readline');
 const { Parser } = require('clyde-transpiler');
+const { Interpreter } = require('../src/index');
 
 function execute() {
   const filename = process.argv[2];
-  const doc = getContent(filename);
+  // TODO
+  // - notify file name not provided
+  // - options to not clear screen
+  // - option to not end dialog and allow retries
+  // - help message
 
-  console.log(doc);
+  const doc = getContent(filename);
+  const dialogue = Interpreter(doc);
+
+  const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+  });
+
+  clearScreen();
+
+  console.log("Press Enter for next line. Type exit to quit.")
+
+  let options;
+
+  rl.on('line', (input) => {
+    if (input === 'exit') {
+      process.exit();
+    }
+
+    clearScreen();
+    if (options && input) {
+      if (isNaN(input) || Number(input) > options.length || Number(input) < 1) {
+        console.log(`Your answer needs to be between 1 and ${options.length}.`)
+        return;
+      } else {
+        dialogue.choose(Math.floor(Number(input) - 1));
+        options = undefined;
+      }
+    }
+
+    const content = dialogue.getContent();
+
+    if (content === undefined) {
+      console.log('-- END');
+      process.exit();
+    }
+
+    if (content.type === 'options') {
+      printOptions(content);
+      options = content.options;
+    } else {
+      printLine(content);
+    }
+  });
 }
+
+const printLine = (line) => {
+  const speaker = line.speaker ? `${line.speaker}: ` : '';
+  console.log(`${speaker}${line.text}`);
+};
+
+const printOptions = (content) => {
+  const speaker = content.speaker ? `${content.speaker}: ` : '';
+  const text = content.name ? `${content.name}` : '';
+  console.log(`${speaker}${text}\n`);
+  content.options.forEach( (option, index) => {
+    console.log(`\t${index + 1} - ${option.label}`);
+  });
+  console.log('');
+};
 
 function getContent(filename) {
   const parser = Parser();
@@ -20,6 +84,11 @@ function getContent(filename) {
   if (extension === 'clyde') {
     return parser.parse(file);
   }
+}
+
+function clearScreen() {
+  readline.cursorTo(process.stdout, 0, 0);
+  readline.clearScreenDown(process.stdout);
 }
 
 
