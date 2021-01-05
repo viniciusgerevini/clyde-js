ee68c2c65d8cca191ca18303784ca6107327dbc1
Vinicius Gerevini <viniciusgerevini@gmail.com>
2021-01-05 16:57:45 +1100

    CLI: implemented compiler with support to dry-run and batching

--- desktop/src/compiler.js
+++ desktop/src/compiler.js
@@ -1,23 +1,33 @@
+const fs = require('fs');
+const { Parser } = require('clyde-parser');
+
 function buildCompilerArgsParser(yargs) {
   return yargs
     .usage(`Usage:\n$0 compile <source file path> <output file path>\n$0 compile -i <input> -o <output>`)
     .check((argv, _options) => {
+      if (argv.batch) {
+        if (argv.batchOutput) {
+          if (argv.batchOutput.length !== argv.batch.length) {
+            throw new Error('ERROR: input and output paths for batch operation should have same number of arguments.')
+          }
+        } else {
+          argv.batchOutput = argv.batch.map(i => outputFilename(i));
+        }
+        return true;
+      }
+
       if (argv._.length === 1 && !argv.input) {
         throw new Error("ERROR: Source file not provided.");
       }
 
       if (!argv.input) {
-        console.log('set')
         argv.input = argv._[1];
       }
       if (!argv.output) {
         if (argv._.length > 2) {
           argv.output = argv._[2];
         } else {
-          argv.output = argv.input.replace(/\.clyde$/, '.json');
-          if (argv.input === argv.output) {
-            argv.output += '.json';
-          }
+          argv.output = outputFilename(argv.input);
         }
       }
 
@@ -38,8 +48,13 @@ function buildCompilerArgsParser(yargs) {
 
     .option('batch', {
       alias: 'b',
-      type: 'string',
+      type: 'array',
       description: 'Compile multiple files at same time'
+    })
+
+    .option('batch-output', {
+      type: 'array',
+      description: 'Path output names for batched files result. Should have same number of arguments as in --batch.'
     })
 
     .option('dry-run', {
@@ -51,13 +66,42 @@ function buildCompilerArgsParser(yargs) {
 }
 
 function executeCompiler(argv, exitCallback) {
+  try {
+    const parser = Parser();
+
+    if (argv.batch) {
+      argv.batch.forEach((input, i) => {
+        compileFile(input, argv.batchOutput[i], argv.d, parser);
+      });
+    } else {
+      compileFile(argv.input, argv.output, argv.d, parser);
+    }
+    // const content = parser.parse(fs.readFileSync(argv.input, 'utf8'));
+    // if (!argv.d) {
+    //   fs.writeFileSync(argv.output, JSON.stringify(content));
+    // }
+    exitCallback();
+  } catch (e) {
+    console.log(`ERROR: ${e.message}`);
+    exitCallback(1);
+  }
   // TODO
-  // compile
   // batch
-  // dry-run
-  console.log('COMPILER');
-  console.log(argv);
-  exitCallback();
+};
+
+const outputFilename = (input) => {
+  let output = input.replace(/\.clyde$/, '.json');
+  if (input === output) {
+    output += '.json';
+  }
+  return output;
+}
+
+const compileFile = (path, output, isDryRun, parser) => {
+  const content = parser.parse(fs.readFileSync(path, 'utf8'));
+  if (!isDryRun) {
+    fs.writeFileSync(output, JSON.stringify(content));
+  }
 };
 
 module.exports = { buildCompilerArgsParser, executeCompiler };
--- desktop/src/compiler.spec.js
+++ desktop/src/compiler.spec.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
 const yargs = require('yargs/yargs');
 
 const { buildCompilerArgsParser, executeCompiler } = require('./compiler');
+
+const SAMPLE_FILE = './test/sample.clyde';
+const SAMPLE_OUTPUT = './test/sample_output.json';
+const OUTPUT_FOLDER = './test/out';
+
+const generateOutputFilename = (suffix) => {
+  return `${OUTPUT_FOLDER}/${Date.now()}${suffix}.json`;
+};
 
 describe('CLI Compiler', () => {
   describe('argument parser', () => {
@@ -67,12 +76,81 @@ describe('CLI Compiler', () => {
         done();
       })
     });
+
+    it('fails if number of arguments in batch is different than arguments in batch-output', (done) => {
+      const args = ['compile', '-b', 'a.clyde', 'b.clyde', '--batch-output', 'a.json'];
+      buildCompilerArgsParser(yargs()).parse(args, (err, argv, _output) => {
+        if (err) {
+          expect(err.message).toEqual('ERROR: input and output paths for batch operation should have same number of arguments.');
+        } else {
+          throw new Error('test should have failed');
+        }
+
+        done();
+      })
+    });
   });
 
   describe('execute', () => {
-    it('calls exit callback when finished', (done) => {
-      const args = { input: 'file.clyde', output: 'file.json' };
-      executeCompiler(args, () => {
+    it('calls exit callback when fails', (done) => {
+      const args = { input: './test/random.clyde', output: generateOutputFilename('do_not_generate') };
+      executeCompiler(args, (errorCode) => {
+        expect(typeof errorCode).toBe('number');
+        done();
+      });
+    });
+
+    it('saves output file when success', (done) => {
+      const args = { input: SAMPLE_FILE, output: generateOutputFilename('success') };
+      executeCompiler(args, (errorCode) => {
+        expect(errorCode).toBe(undefined);
+        expect(fs.readFileSync(args.output, 'utf8')).toEqual(fs.readFileSync(SAMPLE_OUTPUT, 'utf8'));
+        done();
+      });
+    });
+
+    it('does not create output file when dry-run', (done) => {
+      const args = ['compile', SAMPLE_FILE, generateOutputFilename('should_not_exist'), '-d'];
+      const argv = buildCompilerArgsParser(yargs(args)).argv;
+      executeCompiler(argv, (errorCode) => {
+        expect(errorCode).toBe(undefined);
+        expect(fs.existsSync(argv.output)).toBe(false);
+        done();
+      });
+    });
+
+    it('compiles multiple files in batch mode', (done) => {
+      const sample1 = `${OUTPUT_FOLDER}/sample${Date.now()}1.clyde`;
+      const sample2 = `${OUTPUT_FOLDER}/sample${Date.now()}2.clyde`;
+      fs.copyFileSync(SAMPLE_FILE, sample1);
+      fs.copyFileSync(SAMPLE_FILE, sample2);
+      const args = ['compile', '-b', sample1, sample2];
+      const argv = buildCompilerArgsParser(yargs(args)).argv;
+
+      executeCompiler(argv, (errorCode) => {
+        expect(errorCode).toBe(undefined);
+        [sample1, sample2].forEach((sample) => {
+          const output = sample.replace(/\.clyde$/, '.json');
+          expect(fs.readFileSync(output, 'utf8')).toEqual(fs.readFileSync(SAMPLE_OUTPUT, 'utf8'));
+        });
+        done();
+      });
+    });
+
+    it('compiles multiple files in batch mode defining output names', (done) => {
+      const sample1 = `${OUTPUT_FOLDER}/sample${Date.now()}1.clyde`;
+      const sample1Output = `${OUTPUT_FOLDER}/${Date.now()}s1.json`;
+      const sample2Output = `${OUTPUT_FOLDER}/${Date.now()}s2.json`;
+
+      fs.copyFileSync(SAMPLE_FILE, sample1);
+
+      const args = ['compile', '-b', sample1, sample1, '--batch-output', sample1Output, sample2Output];
+      const argv = buildCompilerArgsParser(yargs(args)).argv;
+      executeCompiler(argv, (errorCode) => {
+        expect(errorCode).toBe(undefined);
+        [sample1Output, sample2Output].forEach((output) => {
+          expect(fs.readFileSync(output, 'utf8')).toEqual(fs.readFileSync(SAMPLE_OUTPUT, 'utf8'));
+        });
         done();
       });
     });
